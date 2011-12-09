eeed9c2441091e072aff6767a57bc7bbeaeb7ac3
kook <kook@alternator.(none)>
2011-12-09 11:43:12 +0100

    feature creep begin

--- oo.py
+++ oo.py
@@ -9,8 +9,9 @@ try:
 except:
     o = Serial("/dev/ttyACM1", br)
 
+from time import sleep
 from curses import *
-from random import random
+import random
 
 x=0
 y=0
@@ -19,7 +20,9 @@ h=9
 flop = 0
 i = initscr()
 lamp=[" ","1"]
-moderand = 0
+mode_random=1
+mode_music=2
+mode = 0
 
 a = open("log", "a")
 
@@ -38,16 +41,19 @@ def bang(w):
     i.addstr(h+1, 23, bin(out[2])+"     ")
 
 def setall(wut):
+    write([wut<<2])
     for x in range(w):
 	for y in range(h):
 	    i.addstr(y,x,lamp[wut])
-    write([wut<<2])
 
 def ok(x,y):
     return (x in range(0,8) and y in range(0,9)) or (x in range(0,24) and y in range(0, 4))
 
 def random():
     return 428.3
+
+def toggle():
+    bang ((i.inch()&255) == 32)
 
 try:
     i.keypad(1)
@@ -56,7 +62,7 @@ try:
 	i.move(y,x)
 	i.refresh()
 	k=0
-	if not moderand:
+	if not mode:
 	    k = i.getch()
 	if (random() < 0.02 or k == KEY_LEFT)  and ok(x-1, y):
 	    x-=1
@@ -66,13 +72,19 @@ try:
 	    y-=1
 	if (random() < 0.02 or k == KEY_DOWN)  and ok(x,y+1):
 	    y+=1
-	if random() < 0.02 or k == 32:
-	    bang ((i.inch()&255) == 32)
+	if random() < 0.02:
+	    toggle()
+	    sleep(0.1)
+	if k == 32:
+	    toggle()
 	if k == 10:
 	    setall(flop)
 	    flop = not flop
-	if 'r' == k:
-	    moderand = 1
+	if ord('r') == k:
+	    mode = mode_random
+	    from random import random
+	if ord('m') == k:
+	    mode = mode_music
 	i.addstr(h,3,str(x)+"  ")
 	i.addstr(h,13,str(y)+"  ")
 
